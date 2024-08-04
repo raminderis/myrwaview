0425e0cb84d4d4b8f689d7823d1c9a31f4a10d93
Raminder Singh <raminder.singh@spirent.com>
2024-08-04 09:21:14 -0700

    code base

--- main.go
+++ main.go
@@ -5,11 +5,44 @@ import (
 	"net/http"
 )
 
-func thisIsHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Welcome to my awesome website!</h1>")
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Contact Page</h1><p> To get in touch with me, email me at: <a href='mailto:rami@live.com'>rami@live.com</a></p>")
+
 }
 
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Home Page</h1><p> Here we will have some fun with Real World Assets from On Chain</p>")
+
+}
+
+type Router struct {
+}
+
+func (x Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		http.Error(w, "Page Not Found", http.StatusNotFound)
+	}
+}
+
+// func pathHandler(w http.ResponseWriter, r *http.Request) {
+// 	switch r.URL.Path {
+// 	case "/":
+// 		homeHandler(w, r)
+// 	case "/contact":
+// 		contactHandler(w, r)
+// 	default:
+// 		http.Error(w, "Page Not Found", http.StatusNotFound)
+// 	}
+// }
+
 func main() {
-	http.HandleFunc("/", thisIsHandler)
-	http.ListenAndServe(":3000", nil)
+	var router Router
+	http.ListenAndServe(":3000", router)
 }
